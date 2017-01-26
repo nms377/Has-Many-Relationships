75615f2beea8c8f983b37c3f64b865fe2eda3331
nms377 <nms377@nyu.edu>
2017-01-25 20:14:37 -1000

    Finished setting up the tables.

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -6,6 +6,8 @@ DROP DATABASE IF EXISTS "has_many_blogs";
 
 CREATE DATABASE "has_many_blogs" OWNER "has_many_user";
 
+\i scripts/blog_data.sql;
+
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
@@ -13,8 +15,8 @@ CREATE TABLE users(
 	username varchar(90) NOT NULL,
 	first_name varchar(90) NULL,
 	last_name varchar(90) NULL,
-	created_at timestamp WITHOUT TIME ZONE NOT NULL DEFAULT now(),
-	updated_at timestamp WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+	created_at timestamp WITH TIME ZONE NOT NULL DEFAULT now(),
+	updated_at timestamp WITH TIME ZONE NOT NULL DEFAULT now(),
 	PRIMARY KEY (id)
 );
 
@@ -25,17 +27,22 @@ CREATE TABLE posts(
 	title varchar(180) NULL,
 	url varchar(510) NULL,
 	content text NULL,
-	created_at timestamp WITHOUT TIME ZONE NOT NULL DEFAULT now(),
-	updated_at timestamp WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+	created_at timestamp WITH TIME ZONE NOT NULL DEFAULT now(),
+	updated_at timestamp WITH TIME ZONE NOT NULL DEFAULT now(),
+	users_id SERIAL REFERENCES users (id),
 	PRIMARY KEY (id)
 	);
 
 DROP TABLE IF EXISTS comments;
 
 CREATE TABLE comments(
-	id serial NOT NULL,
+	id serial NOT NULL REFERENCES users (id),
 	body varchar(510) NULL,
-	created_at timestamp WITHOUT TIME ZONE NOT NULL DEFAULT now(),
-	updated_at timestamp WITHOUT TIME ZONE	NOT NULL DEFAULT now(),
+	created_at timestamp WITH TIME ZONE NOT NULL DEFAULT now(),
+	updated_at timestamp WITH TIME ZONE	NOT NULL DEFAULT now(),
+	users_id SERIAL REFERENCES users (id),
+	posts_id SERIAL REFERENCES posts (id),
 	PRIMARY KEY (id)
 );
+
+SELECT 
